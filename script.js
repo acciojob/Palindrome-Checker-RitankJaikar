784d728ca7880c2f4b5e6850b222cb1e990d0728
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-25 12:47:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
+  // Normalize the string: convert to lowercase and remove non-alphanumeric characters
+  const normalizedStr = str.toLowerCase().replace(/[^a-z0-9]/gi, '');
 
+  // Check if the normalized string is equal to its reverse
+  return normalizedStr === normalizedStr.split('').reverse().join('');
 }
 module.exports = palindrome
